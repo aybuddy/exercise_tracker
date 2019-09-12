7e4ba1475b423d736a851ff366563d49a47fa99e
Aaron <ayybuddyy@gmail.com>
2019-09-12 00:03:53 -0400

    Fix bug
    Have nodemon running properly. Fix type in server.js. Server is
    up and running.

--- exercise-tracker/backend/server.js
+++ exercise-tracker/backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
-require('dontenv').config();
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
