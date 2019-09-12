14b6b427008a771d560d3389491f948622254851
Aaron <ayybuddyy@gmail.com>
2019-09-12 01:09:07 -0400

    Add models, routes folder
    Add models folder with exercise and user models. Add routes folder
    with exercise and user routes.

---
+++ exercise-tracker/backend/models/user.model.js
@@ -0,0 +1,22 @@
+const mongoose = require('moongoose');
+
+const Schema = moongoose.Schema;
+
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+			trim: true,
+			minLength: 3
+		}
+	},
+	{
+		timestamps: true
+	}
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
